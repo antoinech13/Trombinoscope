87d4e5f014bc58a67e331da239362b03bdf086a3
antoine <antoine-spartan@hotmail.fr>
2021-01-10 02:32:37 +0100

    test

--- app/src/main/java/com/example/trombinoscope/MainActivity.java
+++ app/src/main/java/com/example/trombinoscope/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.trombinoscope;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.example.trombinoscope.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         this.ed = (Button) findViewById(R.id.éditer);
         this.exp = (Button) findViewById(R.id.exporter);
 
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivityForResult(i, 123);
+
+
         add.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
                 Intent addAct = new Intent(getApplicationContext(), AddAct.class);
@@ -29,5 +36,23 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 123) {
+            if(resultCode == Activity.RESULT_OK){
+
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                Intent i = new Intent(this, LoginActivity.class);
+                startActivityForResult(i, 123);
+            }
+        }
     }
 }
